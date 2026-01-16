c1b039e100f72582bc836bd2956e0f9f2470b222
Lucky2307 <alana.luki@gmail.com>
2026-01-17 00:44:54 +0700

    feat: add version flag

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use clap::{Parser, Subcommand};
 use dotenv::dotenv;
-use ffmpeg_sidecar::paths::ffmpeg_path;
+use ffmpeg_sidecar::{ffprobe::ffprobe_path, paths::ffmpeg_path};
 mod commands;
 mod config;
 mod spinner;
 mod token;
 use crate::commands::{login::login, logout::logout, upload::upload};
 
+fn get_ffmpeg_version() -> String {
+    std::process::Command::new("ffmpeg")
+        .arg("-version")
+        .output()
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap()
+}
+
 #[derive(Parser, Debug)]
+#[command(version)]
 struct Cli {
+    #[arg(long)]
+    ffmpeg_version: bool,
+
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -23,10 +36,17 @@ fn main() {
     dotenv().ok();
     ffmpeg_path();
     let cli = Cli::parse();
+    if cli.ffmpeg_version {
+        println!("Ffmpeg Path: {}", ffmpeg_path().display());
+        println!("Ffprobe Path: {}", ffprobe_path().display());
+        println!("{}", get_ffmpeg_version());
+        return;
+    }
     let command_result = match cli.command {
-        Commands::Login { code } => login(&code),
-        Commands::Logout => logout(),
-        Commands::Upload { file_path } => upload(&file_path),
+        Some(Commands::Login { code }) => login(&code),
+        Some(Commands::Logout) => logout(),
+        Some(Commands::Upload { file_path }) => upload(&file_path),
+        None => Ok("No command provided.".to_string()),
     };
     match command_result {
         Ok(message) => println!("{}", message),
